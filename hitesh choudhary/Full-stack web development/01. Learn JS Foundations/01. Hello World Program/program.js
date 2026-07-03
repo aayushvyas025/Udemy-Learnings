231c456d9cc4f42d5f3fda834e70e63fa8a2a28c
Aayush Vyas <aayush25vyas@gmail.com>
2026-07-03 19:41:12 +0530

    Printing the first program in javascript

---
+++ hitesh choudhary/Full-stack web development/01. Learn JS Foundations/01. Hello World Program/program.js
@@ -0,0 +1,5 @@
+// First program in Javascript 
+console.log('Hello World'); 
+console.log('My name is Aayush Vyas'); 
+console.log('Learning JS');
+// console.log function in javascript is used to print any thing in the terminal or standard output screen 
