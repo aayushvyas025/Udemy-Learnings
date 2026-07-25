c86859d4479abac47fcaa44f1846980688b17489
Aayush Vyas <aayush25vyas@gmail.com>
2026-07-25 16:03:40 +0530

    Introduction to conditional statements

---
+++ hitesh choudhary/Full-stack web development/01. Learn JS Foundations/09. Five Challenges on conditionals/program.js
@@ -0,0 +1,8 @@
+//* Conditionals 
+//? What are conditional statements ?  
+// - conditional statements are used to create our program more controllabel 
+// - With conditionals statements we can create our program more logical hold 
+// - With help of conditional statements we can add conditional check on it. 
+// - After conditional checks we have multiple routes what logic we have to execute in our program or we can add multiple checks to 
+
+//? Let's understand conditional statement with challenges  
