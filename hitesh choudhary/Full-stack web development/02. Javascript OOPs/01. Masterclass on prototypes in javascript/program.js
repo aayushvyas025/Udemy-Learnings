fc27b9b8f346c30d3bd1d2ddd8d460312826e188
Aayush Vyas <aayush25vyas@gmail.com>
2026-08-03 15:53:46 +0530

    Introduction to Prototype and Object Oriented Programming in Javascript

--- hitesh choudhary/Full-stack web development/02. Javascript OOPs/01. Masterclass on prototypes in javascript/program.js
+++ hitesh choudhary/Full-stack web development/02. Javascript OOPs/01. Masterclass on prototypes in javascript/program.js
@@ -1,1 +1,31 @@
 //* Masterclass on prototypes in Javascript 
+// - Every programming language offers the way of writing code like object oriented programming, functional programming, procedural programming etc.   
+//! - Javascript is based on prototype programming language under the hood but another programming language provides the object oriented programming features   
+// - So javascript developers also create the syntactical sugar syntax of `Object Oriented Programming` features with it's method to it so every other language developers also feel not complex to build software in it under the hood javascript is prototype based only. 
+
+//? What is Object Oriented Programming  
+/** 
+ * - The `Object Oriented Programming` is programming paradigm to write and manage our software with more scalability and easy to maintain bigger level of software.  
+ * - `Object Oriented Programming` have two main essential components  
+ *    1. Classes             2. Objects  
+ * * Classes 
+ * Classes are the blueprint of objects and decide the properties and methods are implemented for our objects.  
+ * * Objects 
+ * Objects are the instance of our classes and they are real entity  which interact with each other. 
+ */ 
+
+/** 
+ * ! Javascript under the hood is prototype based language 
+ * - Javascript is prototype based or simply pure object based language but the developers who comes from other programming languages experienced not get some difficulty to create application faster  
+ * - Javascript developers added the syntactical sugar of `Object Oriented Programming` so that all the features and methods feels like the traditional Object-Oriented Programming language but under the hood it's work as prototype based only this classes are wrapper over the prototypes  
+ * 
+ * * Understand with below flow chard diagram  
+ * 
+ *         Prototype_____________________________________________________________________________________                                                                                                                                  |
+ *        |                                                                                    |
+ *        |                                                                                    |
+ *        |                                                                                    |
+ *        |___________________ Javascript Programming  ---------------------------- Object Oriented Programming 
+ *                                Language                                               Paradigm
+ * * Object Oriented Programming is sugar syntactical syntax over the prototype  
+ */
