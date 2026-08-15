2fe7338e77ab7cdf1afa56dbf9a2054db8232e44
Aayush Vyas <aayush25vyas@gmail.com>
2026-08-15 20:07:43 +0530

    Todo List Project Completed

--- hitesh choudhary/Full-stack web development/05. Projects in Javascript/Project 01 - Todo List with Local-Storge/script.js
+++ hitesh choudhary/Full-stack web development/05. Projects in Javascript/Project 01 - Todo List with Local-Storge/script.js
@@ -1,4 +1,64 @@
-document.addEventListener('DOMContentLoaded', function(){
-    console.log("DOM Content Loaded Successfully!!!"); 
-    
-})
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM Content Loaded Successfully!!!");
+
+  const input = document.getElementById("todo-input");
+  const addTodoBtn = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todos.forEach((todo) => renderTodo(todo));
+
+  addTodoBtn.addEventListener("click", function (event) {
+    const inputText = input.value.trim();
+    if (!inputText) {
+      alert("Enter a valid Todo");
+      return;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isCompleted: false,
+    };
+
+    todos.unshift(newTodo);
+    saveTodo();
+    renderTodo(newTodo); 
+    input.value = "";
+  });
+
+  function saveTodo() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }; 
+
+  function renderTodo(todo) {
+    const todoItem = document.createElement("li");
+    todoItem.setAttribute("data-id", todo.id);
+
+    if (todo.isCompleted) {
+      todoItem.classList.add("completed");
+    }
+
+    todoItem.innerHTML = ` 
+      <span>${todo.text}</span> 
+      <button>delete</button>
+    `;
+
+    todoItem.addEventListener("click", (event) => {
+      if (event.target.tagName === "BUTTON") {
+        return;
+      }
+      todo.isCompleted = !todo.isCompleted;
+      todoItem.classList.toggle("completed");
+      saveTodo();
+    });
+
+    todoItem.querySelector('button').addEventListener("click", function(event){
+      event.stopPropagation();  
+      todos = todos.filter((t) => t.id !== todo.id);
+       todoItem.remove();
+      saveTodo(); 
+    });
+
+    todoList.prepend(todoItem);
+
+  }  
+});
