ba6a64fbd23876076b4fedcc41d29b1b46b87181
Aayush Vyas <aayush25vyas@gmail.com>
2026-08-30 19:05:33 +0530

    Adding the localStorage functionality to cart of e-commerce project

--- hitesh choudhary/Full-stack web development/05. Projects in Javascript/Project 03 - E Commerce App/script.js
+++ hitesh choudhary/Full-stack web development/05. Projects in Javascript/Project 03 - E Commerce App/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Dom content loaded successfully!!!");
   // dom elements
   const cartItems = document.getElementById("cart-items");
   const productsList = document.getElementById("product-list");
@@ -10,63 +9,70 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // features
 
-     const products = [
-        { id: 1, name: "Product 1", price: 300 },
-        { id: 2, name: "product 2", price: 500 },
-        { id: 3, name: "product 3", price: 700 },
-        { id: 4, name: "product 4", price: 900 },
-      ]; 
+  const products = [
+    { id: 1, name: "Product 1", price: 300 },
+    { id: 2, name: "product 2", price: 500 },
+    { id: 3, name: "product 3", price: 700 },
+    { id: 4, name: "product 4", price: 900 },
+  ];
 
-     let cart = JSON.parse(localStorage.getItem("cart")) || [],
-   
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
   function AddToCartItems(product) {
     if (!product) return;
+
+    const productExists = cart.some((pdt) => {
+      return pdt.id === product.id;
+    });
+
+    if (productExists) {
+      alert("Product already exists");
+      return;
+    }
+
     cart.push(product);
     saveCart();
-    renderCart(cart);
+    renderCart();
   }
 
   function renderEmptyCart(cart) {
     if (cart.length === 0) {
       emptyCartMessage.classList.remove("hidden");
       cartTotalMessage.classList.add("hidden");
-      cartTotalPrice.innerText = 0.00;
+      cartTotalPrice.innerText = 0.0;
       return;
     }
 
     emptyCartMessage.classList.add("hidden");
-    cartTotalMessage.classList.remove("hidden")
+    cartTotalMessage.classList.remove("hidden");
   }
 
   function saveCart() {
     localStorage.setItem("cart", JSON.stringify(cart));
   }
 
-  function renderCart(cart) {
+  function renderCart() {
     if (!cart) return;
     let totalPrice = 0;
     cartItems.innerHTML = "";
     renderEmptyCart(cart);
     cart.forEach((item, index) => {
-      console.log(item);
       totalPrice += item.price;
       const cartItem = document.createElement("div");
-      cartItem.classList.add('cart-item'); 
+      cartItem.classList.add("cart-item");
       cartItem.innerHTML = `<span>${item.name} - ${item.price.toFixed(2)}</span> <button data-id=${item.id}>Delete</button>`;
       cartItems.appendChild(cartItem);
-      const deleteBtn = cartItem.querySelector('button'); 
-      deleteBtn.addEventListener('click', function(event){
-        if(event.target.tagName === "BUTTON") {
-            console.log("product deleted")
-            const itemId = parseInt(event.target.getAttribute("data-id")); 
-            cart= cart.filter(product => product.id !== itemId);  
-            saveCart(); 
-            renderCart(cart);  
+      const deleteBtn = cartItem.querySelector("button");
+      deleteBtn.addEventListener("click", function (event) {
+        if (event.target.tagName === "BUTTON") {
+          const itemId = parseInt(event.target.getAttribute("data-id"));
+          cart = cart.filter((product) => product.id !== itemId);
+          saveCart();
+          renderCart();
         }
-      })
+      });
     });
-    cartTotalPrice.innerText = totalPrice;
+    cartTotalPrice.innerText = totalPrice.toFixed(2);
   }
 
   function renderProducts() {
@@ -80,6 +86,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (event.target.tagName === "BUTTON") {
           const productId = parseInt(event.target.getAttribute("data-id"));
           const product = products.find((product) => product.id === productId);
+
           AddToCartItems(product);
         }
       });
@@ -87,10 +94,10 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   renderProducts();
+  renderCart();
 
   // event listener
   checkoutBtn.addEventListener("click", function () {
     alert(`Your Total Bill is $${cartTotalPrice.innerText}`);
   });
-  console.log(cart); 
 });
